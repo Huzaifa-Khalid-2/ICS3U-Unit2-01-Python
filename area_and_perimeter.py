55273aba9d43d95f22327246cb90ae8c878abda7
Huzaifa Khalid <huzaifa.khalid@mths.ca>
2022-02-25 19:53:36 +0000

    added area and perimeter5

--- area_and_perimeter.py
+++ area_and_perimeter.py
@@ -11,7 +11,7 @@ import math
 def main():
     # this function calculates the area and perimeter
 
-    print("If the circles has a radius of 15 mm: ")
+    print("If the circle has a radius of 15 mm: ")
     print("")
     print("Area is {} mm².".format(math.pi * 15**2))
     print("Perimeter is {} mm.".format(2 * math.pi * 15))
